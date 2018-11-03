697877be0d89c23526b0eb45667cc055328edcaa
codeauslander <cesar.gusto.catano@gmail.com>
2018-11-03 00:03:44 -0500

    Attemp duplicated_claim

--- lib/adjudication/engine.rb
+++ lib/adjudication/engine.rb
@@ -6,12 +6,20 @@ require "adjudication/engine/claim"
 module Adjudication
   module Engine
     def self.valid_NPI(npi)
-      npi && npi.length == 10 && npi.scan(/\D/).empty? 
+      npi && npi.scan(/\D/).empty? && npi.length == 10
     end
 
-    def self.claim_match_provider(claim, providers)
-      claim["npi"]
+    def self.match_provider(claim, normalize_provider_data)
+      normalize_provider_data.each do |provider| 
+        if claim["npi"] == provider[2]
+          claim["provider"] = provider.to_h
+          return claim 
+        end
+      end
+      return false
     end
+
+
 
     def self.run claims_data
       fetcher = Adjudication::Providers::Fetcher.new
@@ -21,17 +29,17 @@ module Adjudication
       # provider NPI (national provider ID), and run the adjudicator.
       # This method should return the processed claims
 
-      normalize_provider_data = provider_data.select {|provider| valid_NPI(provider[2]) ? provider :  (STDERR.puts "Log bad NPI's to STDERR #{provider[2]}") }
+      normalize_provider_data = provider_data.select {
+        |provider| valid_NPI(provider[2]) ? 
+          provider :  
+          (STDERR.puts "Log bad NPI's to STDERR #{provider[2]}")
+      }
       
+      processed_claims = claims_data.map {
+        |claim| Claim.new(match_provider(claim, normalize_provider_data)) if match_provider(claim, normalize_provider_data)
+      }.compact
 
-      # processed_claims = []
-      # claims_data.each do |claim|
-      #   if claim_match_provider(claim, normalize_provider_data)
-      #     processed_claims << claim
-      #   end
-      # end
-      p normalize_provider_data.length
-
+      processed_claims
     end
   end
 end
--- lib/adjudication/engine/adjudicator.rb
+++ lib/adjudication/engine/adjudicator.rb
@@ -7,10 +7,37 @@ module Adjudication
         @processed_claims = []
       end
 
+      def duplicated_claim(claim, processed_claims)
+        processed_claims.each do |processed_claim| 
+          if  processed_claim.start_date == claim.start_date &&
+              processed_claim.patient["ssn"] == claim.patient["ssn"] 
+          # processed_claim.patient["ssn"] == claim.patient["ssn"] &&
+              p processed_claim.line_items
+              return true
+          end
+
+        end
+        return false
+      end
+
 
       def adjudicate(claim)
         # TODO implement adjudication rules, and add any processed claims (regardless
         # of status) into the processed_claims attribute.
+
+        # 1. As you can see in the claims JSON fixture, a claim is an instance of a visit to the dentist. It has line items, which are individual services billed by the dentist. Each line item or service is adjudicated by itself, and can be rejected or paid by itself.
+        # 2. Reject any duplicate claims. A duplicate claim is defined as having the same start date, patient SSN, and set of procedures codes as another claim. All but the first claim in a duplicate set should be rejected.
+        # 3. If the provider was not successfully matched, reject the claim and all of its line items. This plan does not cover out of network at all. :(
+        # 4. Fully pay any preventive and diagnostic codes. Luckily, your coworker already provided a helper method on the `ClaimLineItem` model to help you figure this out.
+        # 5. Pay any orthodontic line item at 25% of the charged rate. Again, your coworker added a helper method on `ClaimLineItem` to help you with this.
+        # 6. Reject anything else.
+        # 7. Log any rejections to STDERR.
+
+        # same start date, patient SSN, and set of procedures codes as another claim
+        duplicated_claim(claim)
+
+        
+
       end
     end
   end
